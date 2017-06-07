ad7d6f8fe2e39f14e2419d268bcac8f8bdddb1fe
DavidGWheeler <wots_uh_thedeal@msn.com>
2017-06-06 23:35:22 -0700

    add all the things

---
+++ lab-davidw/test/home-controller-test.js
@@ -0,0 +1,28 @@
+'use strict';
+
+// const angular = require('angular');
+// require('angular-mocks');
+
+describe('testing HomeController', function() {
+  beforeEach(() => {
+    angular.mock.module('cfgram');
+    angular.mock.inject(($rootScope, $controller) => {
+      this.$rootScope = $rootScope;
+      this.homeCtrl = new $controller('HomeController');
+    });
+  });
+
+  afterEach(() => this.$rootScope.$apply());
+
+  it('should exist', () => {
+
+    expect(this.homeCtrl).toBeDefined();
+  });
+
+  it('should have a title', () => {
+
+    expect(this.homeCtrl.title).toBeDefined();
+    expect(this.homeCtrl.title).toEqual(jasmine.any(String));
+    expect(this.homeCtrl.title).toBe('Welcome to Home');
+  });
+});
