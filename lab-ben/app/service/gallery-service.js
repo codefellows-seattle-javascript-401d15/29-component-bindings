9ea55bb3410d2701ee5c7feba3c4e506c34399de
Benjamin <ayzenbergb@gmail.com>
2017-06-07 10:33:48 -0700

    added nav bar functionality

--- lab-ben/app/service/gallery-service.js
+++ lab-ben/app/service/gallery-service.js
@@ -61,6 +61,61 @@ module.exports = [
         return $q.reject(err);
       });
     };
+
+    service.updateGallery = (galleryId, gallery) => {
+      $log.debug('galleryService.updateGallery');
+
+      return authService.getToken()
+      .then(token => {
+        let url = `${__API_URL__}/api/gallery/${galleryId}`;
+        let config = {
+          headers: {
+            Accept: 'application/json',
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        };
+        return $http.put(url, gallery, config);
+      })
+      .then(res => {
+        service.galleries.forEach((ele, idx) => {
+          if(ele._id === res.data._id) service.galleries[idx] = res.data;
+        });
+        return res.data;
+      })
+      .catch(err => {
+        $log.error(err.message);
+        $q.reject(err);
+      });
+    };
+
+    service.deleteGallery = (galleryId) => {
+      $log.debug('galleryService.deleteGallery');
+
+      return authService.getToken()
+      .then(token => {
+        let url = `${__API_URL__}/api/gallery/${galleryId}`;
+        let config = {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            Accept: 'application/json',
+          },
+        };
+
+        return $http.delete(url, config);
+      })
+      .then(res => {
+        service.galleries.forEach((ele, idx) => {
+          if(ele._id === galleryId) service.galleries.splice(idx, 1);
+        });
+        return res.data;
+      })
+      .catch(err => {
+        $log.error(err.message);
+        return $q.reject(err);
+      });
+    };
+    
     return service;
   },
 ];
