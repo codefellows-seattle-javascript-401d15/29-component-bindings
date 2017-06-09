190326e8067325a6936b4782e19d9297bf780693
Ithalis <jt.2012@yahoo.com>
2017-06-09 11:03:59 -0700

    yay

---
+++ lab-james/app/component/gallery/edit-gallery/edit-gallery.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  template: require('./edit-gallery.html'),
+  controllerAs: 'editGalleryCtrl',
+  bindings: {
+    gallery: '<',
+  },
+  controller: ['$log', 'galleryService', '$rootScope', function($log, galleryService, $rootScope) {
+    this.$onInit = () => {
+      $log.debug('edit-gallery controller');
+
+      this.updateGallery = () => {
+        galleryService.updateGallery(this.gallery)
+        .then(
+          () => {
+            $log.log('Updated!');
+            $rootScope.$on('locationChangeSuccess', this.fetchGalleries);
+          }
+        )
+        .catch(err => $log.error(err));
+      };
+    };
+  }],
+};
